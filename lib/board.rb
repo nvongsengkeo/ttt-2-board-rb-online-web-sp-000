2836bd7f7f5e22a6b99c023402db462f92e6994c
Nancy Vongsengkeo <nancyvongsengkeo@gmail.com>
2020-02-26 22:27:06 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,29 @@
-# Define the variable board below.
+# Define the variable board below
+board = ["1","2","3","4","5","6","7","8","9"]
+
+#Position 1: Left 
+board[1]
+
+#Position 2: Middle
+board[2]
+
+#Position 3: Right
+board[3]
+
+#Position 4: Left 
+board[4]
+
+#Position 5: Middle
+board[5]
+
+#Position 6: Right
+board[6]
+
+#Position 7: Left 
+board[7]
+
+#Position 8: Middle 
+board[8]
+
+#Position 9: Right
+board[9]
